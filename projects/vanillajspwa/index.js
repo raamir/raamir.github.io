b45c1c1daaefd6a1ecadf239f443998fd776687b
Rija Aamir <aamir.rija@gmail.com>
2019-03-08 16:14:53 -0600

    Update index.js

--- projects/vanillajspwa/index.js
+++ projects/vanillajspwa/index.js
@@ -2,7 +2,7 @@
 
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('/vanillajspwa/sw.js')
+           .register('/projects/vanillajspwa/sw.js')
            .then(function() { console.log('Service Worker Registered'); });
 }
 
