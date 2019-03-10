5d1015a61d6d6cd3ed9936830d354bdea1a0484e
Rija Aamir <aamir.rija@gmail.com>
2019-03-10 17:29:34 -0500

    Add files via upload

---
+++ projects/quizpwa/chicago.js
@@ -0,0 +1,89 @@
+// Ivonne Garcia, Nina Keoborakot, Rija Aamir, Irandy Perez
+// This is the jquery file that adds functionality to our Chicagoan quiz.
+
+
+// jQuery for questions.html
+$(function() {
+    var count = 0;
+    $(".video").hide();
+    $(".imgResult").hide();
+    
+    $(".correctAns").click(function(){
+         count++;
+         console.log("Counter has been incremented!")
+    });
+  
+    $("#checkButton").click(function () {
+        $("#feedback").show();
+        $("#checkButton").hide();
+        $("#result").css({
+            "background-color": "powderblue",
+            "color": "black",
+            "border-radius" : 5,
+            "width" : 600,
+            "text-align": "center",
+            "font-family": "'Comic Sans MS', cursive, sans-serif",
+            "font-size" : 20
+        });
+        
+        if (count >= 3) {
+            $("#result").show().html("You got " + count + " answers correct. <br/> You are a true Chicagoan!" + "<br/>" + "Click the squares below to reveal a celebratory image.<br/>");
+            console.log("Hide video")
+            $(".video").hide();
+            $(".imgResult").show();
+            $(".target").css("cursor", "pointer");
+            
+            $("#cover1").click(function(){
+            console.log("Revealed one square");
+            $("#cover1").html("<img src='images/result1.png'>");
+            });
+
+            $("#cover2").click(function(){
+            console.log("Revealed one square");
+            $("#cover2").html("<img src='images/result2.png'>");
+            });
+
+            $("#cover3").click(function(){
+            console.log("Revealed one square");
+            $("#cover3").html("<img src='images/result3.png'>");
+            });
+
+            $("#cover4").click(function(){
+            console.log("Revealed one square");
+            $("#cover4").html("<img src='images/result4.png'>");
+            });
+        }
+      
+        else {
+            $("#result").show().html("You got " + count + " answers correct.<br/> You should probably watch this video..<br/>");
+            console.log("Show video");
+            $(".video").show();
+        }
+    });
+    
+    // jQuery for feedback.html
+    $(".smiley").css("cursor", "pointer"); 
+    $("#dialog").dialog({
+        height: 250,
+        width: 350,
+        autoOpen: false,
+        modal: true,
+        draggable: false,
+        resizable: false,
+        buttons : {
+            "Re-Take Quiz" : function() {
+                location.replace("home.html");            
+            },
+         
+            "No" : function() {
+                $(this).dialog('close');
+            }
+        }
+    });
+
+  $("#submit").click(function(e) { 
+      e.preventDefault();
+      $("#dialog").dialog("open");
+  });
+});
+
---
+++ projects/quizpwa/sw.js
@@ -0,0 +1,49 @@
+let CACHE_NAME = 'quizpwa';
+let urlsToCache = [
+	'home.html',
+	'feedback.js',
+	'questions.html',
+    'index.js',
+	'chicago.js',
+	'chicago.css',
+	'images/background.jpg',
+    'images/cover1.png',
+    'images/cover2.png',
+    'images/cover3.png',
+    'images/cover4.png',
+    'images/emoji1.png',
+    'images/emoji2.png',
+    'images/result1.png',
+    'images/result2.png',
+    'images/result3.png',
+    'images/result4.png',
+    'images/true.jpg',
+    'images/disney.png',
+    'manifest.webmanifest'
+    
+];
+
+self.addEventListener('install', function(event) {
+	// Perform install steps
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+			.then(function(cache) {
+				console.log('Opened cache');
+				return cache.addAll(urlsToCache);
+			}) 
+	);
+});
+
+
+// when the browser fetches a url, either response with
+// the cached object or go ahead and fetch the actual url
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    // ensure we check the *right* cache to match against
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(event.request).then(res => {
+        return res || fetch(event.request)
+      });
+    })
+  );
+});
