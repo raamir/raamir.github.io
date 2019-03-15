c8356a5cbb77c48bac56d826ccb064b1e76a3fdf
raamir <raamir@mail.depaul.edu>
2019-03-14 23:05:57 -0400

    My Portfolio

---
+++ myportfoliosite/src/components/contact.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
+
+
+
+class Contact extends Component {
+  render() {
+    return(
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Rija Aamir</h2>
+            <img
+              src="https://image.flaticon.com/icons/png/512/219/219969.png"
+              alt="avatar"
+              style={{height: '250px'}}
+               />
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr/>
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-phone-square" aria-hidden="true"/>
+                    (630) 965-9736
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-fax" aria-hidden="true"/>
+                    (630) 965-9736
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent style={{fontSize: '30px', fontFamily: 'Anton'}}>
+                    <i className="fa fa-envelope" aria-hidden="true"/>
+                    aamir.rija@gmail.com
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    )
+  }
+}
+export default Contact;
